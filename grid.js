d91df7df5f186888a2e11fef1482b332bb4ec236
Carter2307 <remirjunior1999@gmail.com>
2021-11-09 22:42:21 -0400

    Manage little details

--- grid.js
+++ grid.js
@@ -13,21 +13,26 @@ function gridSystem(cardWidth, cardNumber) {
 		gridHandler("input", defaultSize, defaultCard);
 	};
 
-	//Valide if params exsit
+	//Valide if params exsit and run grid handler function 
 	if (width && number && gridContainer) {
 		gridHandler("nothing", width, number);
 	}
 
+	//create element to grid container
 	function createGridCard() {
 		const card = document.createElement("DIV");
 		card.setAttribute("class", "grid__card");
 		return card;
 	}
 
+	//Reset and show grid chil element 
 	function gridHandler(container, size, num) {
-		Array.from(document.querySelectorAll(`${container}`) || gridContainer).forEach((el) => (el.value = ""));
+	
 		if (container) {
-			Array.from(gridContainer.children).forEach((child) => gridContainer.removeChild(child));
+			console.log(container)
+			Array.from(gridContainer.children)
+				.forEach((child) => gridContainer.removeChild(child));
+			
 			gridContainer.style.gridTemplateColumns = `repeat(auto-fill, minmax(${size}px, 1fr))`;
 			for (let i = 0; i < num; i++) {
 				gridContainer.appendChild(createGridCard());
@@ -37,5 +42,3 @@ function gridSystem(cardWidth, cardNumber) {
 }
 
 //=================== Constructor function approch =================//
-
-function Grid(cardWidth, cardNumber) {}
